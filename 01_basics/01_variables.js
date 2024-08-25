8eba17100a7a56b73139ee59a994337435784018
Piyushraj06 <piyushraj577mth@gmail.com>
2024-08-26 01:56:00 +0530

    Add this file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 120705
+let accountEmail = "piyush@gmail.com"
+var accountPassword = "12345"
+accountCity = "Motihari"
+
+// accountId = 2 //it is not allowed
+accountEmail = "piyush123@gmail.com"
+accountPassword = "123"
+accountCity = "champaran"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
