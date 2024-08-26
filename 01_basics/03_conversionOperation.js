6249604cd659742c3bdd77233df3447c7fd5bd7b
Piyushraj06 <piyushraj577mth@gmail.com>
2024-08-27 03:08:56 +0530

    add this file

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+// let score = 33
+
+// let score = "33"//string
+let score = "33abc"
+console.log(typeof score)
+
+let valueInNumber = Number(score)//number
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" = 33
+//"33abc" = NaN
+//true = 1; false =0;
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
