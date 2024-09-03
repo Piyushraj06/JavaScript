c1b3f441e629d688403dfe41775db3808314f36e
Piyushraj06 <piyushraj577mth@gmail.com>
2024-09-04 02:18:08 +0530

    add this

---
+++ 01_basics/07_date.js
@@ -0,0 +1,13 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023,0,23)//zero se start hota hai month format js me
+console.log(myCreatedDate.toDateString());
+
+
