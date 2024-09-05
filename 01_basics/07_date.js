29a0fda287d7462824cda5976195b94488c7acd5
Piyushraj06 <piyushraj577mth@gmail.com>
2024-09-05 23:36:29 +0530

    Add Array file

--- 01_basics/07_date.js
+++ 01_basics/07_date.js
@@ -11,3 +11,18 @@ let myCreatedDate = new Date(2023,0,23)//zero se start hota hai month format js 
 console.log(myCreatedDate.toDateString());
 
 
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());//msec
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
+
+newDate.toLocaleString('default',{
+    weekday: "long"
+    })
+
