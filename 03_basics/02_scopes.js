f970797e40799057fa932e1f7d6fa13fd28e5870
Piyushraj06 <piyushraj577mth@gmail.com>
2024-09-09 02:04:12 +0530

    add thisfile

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,16 @@
+// var jo hai wo block scope ko support nahi karta hai for eg:
+
+// block scope generally har lang me {} hi hotaq hai
+//loop ke ander ka block scope aur baaki bahar ka global scope
+// let a = 300
+if(true){
+    let a = 10;
+    const b = 20;
+    var c = 30;
+    console.log("Inner :" , a);//10
+    
+}
+
+// console.log(a);//error oif loop me hai
+// console.log(b);//error if loop me hai 
+console.log(c);// 30 output jb ki nahi aana chahiye yhi hai block scope
