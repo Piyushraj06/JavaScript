3087402461bd43daee4d9efa09468736f04a61a0
Piyushraj06 <piyushraj577mth@gmail.com>
2024-09-14 01:42:03 +0530

    ADD This

---
+++ 04_controlFlow/lecFour.js
@@ -0,0 +1,15 @@
+//for in loop
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for(const key in myObject){
+    console.log(key); 
+}
+
+//object ke uper for in loop or array ke uper for of loop
+
+
