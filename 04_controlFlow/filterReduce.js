2bb53aff34d0dc0b722e4063f82c823bd1d69648
Piyushraj06 <piyushraj577mth@gmail.com>
2024-09-14 03:30:22 +0530

    add

---
+++ 04_controlFlow/filterReduce.js
@@ -0,0 +1,55 @@
+//Filter
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+// const newNums = myNums.filter( (num) => num > 4)
+// console.log(newNums);
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4 // yha object bn rha hai islie return keyword use krna pdega
+// })
+// console.log(newNums);
+
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNumbers.map((num) => {
+//     return num + 10
+// })
+
+// const newNums = myNumbers.map((num) => num * 10).map((num) => num + 1)
+//                          .filter((num) => num>40)
+
+// console.log(newNums);
+
+
+//Reduce
+
+const myNums = [1,2,3]
+
+// const myTotal = myNums.reduce(function(acc, curVal){
+//     console.log(`acc is: ${acc}, cur value is: ${curVal}`);
+    
+//     return acc+curVal
+// },0)
+
+
+const myTotal = myNums.reduce((acc, curVal) => acc+curVal,0)
+console.log(myTotal);
+
+
+const shoppingCart  = [
+    {
+        itemName: "js course",
+        price: 1499
+    },
+    {
+        itemName: "py course",
+        price: 2499
+    },
+    {
+        itemName: "java course",
+        price: 3499
+    },
+]
+
+const shoppingPrice = shoppingCart.reduce((acc, item) => acc + item.price,0)
+
+console.log(shoppingPrice);
--- 04_controlFlow/forEachLoop.js
+++ 04_controlFlow/forEachLoop.js
@@ -11,3 +11,23 @@ coding.forEach((item) => {
 coding.forEach((item,index,arr) => {
     console.log(item,index,arr);
 })
+
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languagefileName: "js"
+    },
+    {
+        languageName: "java",
+        languagefileName: "java"
+    },
+    {
+        languageName: "python",
+        languagefileName: "py"
+    }
+]
+
+myCoding.forEach( (item) =>{
+    console.log(item.languageName);
+})
